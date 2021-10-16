076f4201ec4adb4abe1aa61f4947717c3c165fc1
Андрей Орлов <orlov.lionel@yandex.ru>
2021-10-16 13:16:57 +0400

    HW TG-Check_homework_bot

--- homework.py
+++ homework.py
@@ -1,14 +1,27 @@
 import logging
+import logging.handlers
+import os
+import sys
+import time
+from logging import StreamHandler
 
-...
+import requests
+import telegram
+from dotenv import load_dotenv
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
+load_dotenv()
 
-...
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[StreamHandler(stream=sys.stdout)]
+)
 
-RETRY_TIME = 300
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+RETRY_TIME = 60 * 10
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 HOMEWORK_STATUSES = {
@@ -18,38 +31,102 @@ HOMEWORK_STATUSES = {
 }
 
 
+class HTTPStatusIsNot200(Exception):
+    """Обрабатывает случай когда HTTP-ответ не равен 200."""
+
+    pass
+
+
+class ApiStatusUndocumented(Exception):
+    """Обрабатывает случай недокументированного статуса ответа от API."""
+
+    pass
+
+
 def send_message(bot, message):
-    ...
+    """При изменении статуса домашки отправляет сообщение пользователю."""
+    bot = telegram.Bot(TELEGRAM_TOKEN)
+    bot.send_message(chat_id=CHAT_ID, text=message)
+    logging.info(f'Сообщение успешно отправлено {message}')
 
 
 def get_api_answer(url, current_timestamp):
-    ...
+    """Отправляет запрос к API домашки на ENDPOINT."""
+    url = ENDPOINT
+    headers = {
+        'Authorization': f'OAuth {PRACTICUM_TOKEN}'
+    }
+    payload = {'from_date': current_timestamp}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=payload
+    )
+    if response.status_code != 200:
+        logging.error(
+            f'HTTPStatus is not OK: {response.status_code}')
+        raise HTTPStatusIsNot200(
+            f'Эндпоинт {ENDPOINT} недоступен. '
+            f'Код ответа API: {response.status_code}')
+    return response.json()
 
 
 def parse_status(homework):
-    verdict = ...
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    """При изменении статуса домашки - анализирует его."""
+    verdict = HOMEWORK_STATUSES.get(homework.get('status'))
+    homework_name = homework.get('homework_name')
+    if verdict is not None:
+        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
-    homeworks = response.get('homeworks')
-    ...
+    """После запроса к API домашки проверяет не изменился ли статус."""
+    homeworks = response.get('homeworks')[0]
+    homework = homeworks.get('status')
+    if homework not in HOMEWORK_STATUSES:
+        logging.error(
+            f'Недокументированный статус домашней работы: {homework}'
+        )
+        raise ApiStatusUndocumented(
+            f'Недокументированный статус домашней работы: {homework}'
+        )
+    return homework
+
+
+def check_tokens():
+    """Проверка наличия переменных окружения."""
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN is None:
+        logging.critical(
+            'Отсутствует обязательная переменная окружения.'
+            'Программа принудительно остановлена.')
+        exit()
 
 
 def main():
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    """Запуск телеграмм-бота."""
+    check_tokens()
+    bot = telegram.Bot(TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    ...
+    errors = True
     while True:
         try:
-            ...
+            response = get_api_answer(ENDPOINT, current_timestamp)
+            if response.get('homeworks') == []:
+                time.sleep(RETRY_TIME)
+                continue
+            check_response(response)
+            message = parse_status(response.get('homeworks')[0])
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
+            current_timestamp = int(time.time() - RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
+            message = f'Сбой в работе телеграмм-бота: {error}'
+            logging.info(f'Уведомление об ошибке отправлено в чат {message}')
+            if errors:
+                errors = False
+                send_message(bot, message)
             time.sleep(RETRY_TIME)
+            current_timestamp = int(time.time() - RETRY_TIME)
             continue
 
 
